efd199096dab133a872bb3c3ad13aa6d505bee79
Tomat7 <star0413@gmail.com>
2018-05-31 14:32:17 +0300

    v141 pressure sensors added

--- functions.ino
+++ functions.ino
@@ -2,11 +2,6 @@
 void setupNetMB()
 {
   //Config Modbus IP
-  /*
-    #ifndef ETHERNET_MACID
-    macID = EEPROM.read(EEPROM.length() - 1);
-    #endif
-  */
   LD.printString_6x8("MAC: ", LCDX1, 1);
   for (byte i = 0; i < 6; ++i)
   {
@@ -25,18 +20,12 @@ void setupNetMB()
     if (i < 5) Serial.print(':');
   }
   Serial.println();
-  /*
-      LD.printString_6x8("cfg_IP: ", lcdX, 2);
-      for (byte i = 0; i < 4; ++i)
-      {
-        LD.printNumber((long)ip[i]);
-        if (i < 3) LD.printString_6x8(".");
-      }
-  */
-#ifdef ETHERNET_DHCP
-  mb.config(mac);
-#else
+
+#ifndef ETHERNET_DHCP
+  const byte ip[] = { ETHERNET_IP };
   mb.config(mac, ip);
+#else
+  mb.config(mac);
 #endif
 
 #ifdef ETHERNET_ENC28J60
@@ -70,46 +59,80 @@ void mbHeartBeat()
   // это будет признаком "живости" Мастера Modbus'а для модуля
   // и наоборот: не 0 в SECONDS - признак "живости" модуля для Мастера
   // хотя Мастеру логичнее отслеживать "живость" по GetQuality
-  //
+
   if (mb.Hreg(hrSECONDS) == 0) mb.Hreg(hrSECONDS, msReinit / 1000);
   if (((uint16_t)(msReinit / 1000) - mb.Hreg(hrSECONDS)) < MB_TIMEOUT)
   {
-    mb.Hreg(hrLAST_DROP, valve[0].lastON);   // сохраняем залёты по времени (если чаще чем клики клапана??)
-    mb.Hreg(hrCLICKS, valve[0].Clicks);      // сохраняем сколько всего накликали
+    //mb.Hreg(hrLAST_DROP, valve[0].lastON);   // сохраняем залёты по времени (если чаще чем клики клапана??)
+    //mb.Hreg(hrCLICKS, valve[0].Clicks);      // сохраняем сколько всего накликали
     LD.printString_12x16("MB_OK", LCDX2, 6);
+    mbMasterOK = true;
   }
-  else                // если мастера нет больше XX секунд - глушим щелкунчика
+  else                // если мастера нет больше MB_TIMEOUT секунд - поднимаем флаг
   {
-    mb.Hreg(hrLAST_DROP, 0);
-    mb.Hreg(hrCLICK_INTERVAL, 0);
+    //mb.Hreg(hrLAST_DROP, 0);
+    //mb.Hreg(hrCLICK_INTERVAL, 0);
     LD.printString_12x16("  OFF", LCDX2, 6);
     Serial.print("Master OFFline ");
     Serial.println(mb.Hreg(hrSECONDS));
+    mbMasterOK = false;
   }
 }
 
 void initDS(int i)
 {
-  sensor[i].init(DS_CONVTIME);
-  mb.addHreg(i);                  // Модбас регистр - значение температуры
-  mb.addHreg(hrSECONDS + i + 1);  // Модбас регистр - длительность преобразования !DEBUG!
-  int pins[] = OWPINS;
-  String dsInfo = "ID " + String(i, DEC) + "|Connected " + String(sensor[i].Connected, DEC);
+  ds18b20[i].init(DS_CONVTIME);
+  mb.addHreg(hrTEMP + i);                // Модбас регистр - значение температуры
+  mb.addHreg(hrDSDEBUG + i);  // Модбас регистр - длительность преобразования !DEBUG!
+  int pins[] = DSPINS;
+  String dsInfo = "ID " + String(i, DEC) + "|Connected " + String(ds18b20[i].Connected, DEC);
   Serial.println(dsInfo);
   LD.printNumber((long)pins[i]);
-  if (i < (sensCount - 1)) LD.printString_6x8(", ");
+  if (i < (dsCount - 1)) LD.printString_6x8(", ");
+}
+
+void initValve(int i)
+{
+  int hrBase = hrVALVE + i * 3;
+  valve[i].init(0, V_OPENTIME);
+  mb.addHreg(hrBase);                // Valve OFF-time - регулирует скорость отбора
+  mb.addHreg(hrBase + 1); // DEBUG?? // длительность крайнего открытия клапана (100 мс + залёты)
+  mb.addHreg(hrBase + 2);            // количество кликов с начала работы (для расчета объема)
+  int pins[] = VALVEPINS;
+  LD.printNumber((long)pins[i]);
+  if (i < (vCount - 1)) LD.printString_6x8(", ");
 }
 
 void handleDS(int i)
 {
-  float t = sensor[i].Temp;
-  mb.Hreg(i, round( t * 100));  // заносим в регистр Modbus (температура * 100)
-  mb.Hreg((hrSECONDS + i + 1), sensor[i].TimeConv);  // DEBUG!
+  float t = ds18b20[i].Temp;
+  mb.Hreg(hrTEMP + i, round( t * 100)); // заносим в регистр Modbus (температура * 100)
+  mb.Hreg(hrDSDEBUG + i, ds18b20[i].TimeConv);  // DEBUG!
   String dsInfo = "DS " + String(i, DEC) + ": " + String(t, 2) + " | parasite: " +
-                  String(sensor[i].Parasite, DEC) + " | " + String(sensor[i].TimeConv, DEC);
+                  String(ds18b20[i].Parasite, DEC) + " | " + String(ds18b20[i].TimeConv, DEC);
   Serial.println(dsInfo);
   dtostrf(t, 4, 1, cbuf);
   LD.printString_12x16(cbuf, 0, (i * 3));
+}
+
+void handleValve(int i)
+{
+  int hrBase = hrVALVE + i * 3;         // адрес регистра в котором хранится длительность стопа клапана
+  if (mbMasterOK)
+  {
+    mb.Hreg(hrBase + 1, valve[i].lastON); // сохраняем залёты по времени (если чаще чем клики клапана??)
+    mb.Hreg(hrBase + 2, valve[i].Clicks); // сохраняем сколько всего накликали
+  } else
+  {
+    //mb.Hreg(hrLAST_DROP, 0);
+    mb.Hreg(hrBase, 0);              // если Мастера нет - прекращаем клацать клапаном
+  }
+  valve[i].setTime(mb.Hreg(hrBase)); // задаем время стопа клапану
+  dtostrf(mb.Hreg(hrBase), 5, 0, cbuf);
+  LD.printString_6x8(cbuf, LCDX2+32, i);   // показываем это время на дисплее
+  String vInfo = "VALVE " + String(i, DEC) + ": off " + String(mb.Hreg(hrBase), DEC) + " ms | lastOn " +
+                 String(valve[i].lastON, DEC) + " ms | clicks: " + String(valve[i].Clicks, DEC);
+  Serial.println(vInfo);
 }
 
 void printFreeRam ()
