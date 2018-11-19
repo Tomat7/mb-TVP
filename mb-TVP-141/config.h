0af06f10dad9d43bba62426516ac1228e23dc31d
Tomat7 <star0413@gmail.com>
2018-11-19 14:13:45 +0300

    all versions added

---
+++ mb-TVP-141/config.h
@@ -0,0 +1,99 @@
+#define SKETCHFILE __FILE__ " "
+#define SKETCHTIME __DATE__ " " __TIME__
+#define SKETCHVERSION SKETCHFILE SKETCHTIME
+
+#define ETHERNET_MAC MAC0, MAC1, MAC2, 0xEE, 0x30, ETHERNET_ID // MAC адрес Ethernet шилда 
+
+#ifndef ETHERNET_DHCP                               // если не используется DHCP
+#define ETHERNET_IP 192, 168, 1, 30 + ETHERNET_ID   // задаём IP адрес Ethernet модуля 
+#endif
+/*
+   к младшему байту прибавляется ETHERNET_ID, то есть при ETHERNET_ID==1, IP будет 192.168.1.31
+   адрес должен быть из диапазона сети в которой планируется использовать модуль, и не должен совпадать
+   с адресом любого другого устройства в сети. посмотреть IP адреса сети можно командой ipconfig /all (Windows)
+   или ifconfig (Linux) с ПК подключенного и корректно работающего в сети - смотреть на строки
+   "IPv4 Address" или "inet addr". но лучше всего залить тестовый скетч для соответствующего модуля
+   и посмотреть монитор порта - DHCP должен выдать адрес. это еще и подтвердит работоспособность модуля.
+   File->Examples->Ethernet-> DhcpAddressPrinter для w5100
+   File->Examples->EtherCard-> testDHCP для enc28j60
+
+   https://ru.wikipedia.org/wiki/%D0%A7%D0%B0%D1%81%D1%82%D0%BD%D1%8B%D0%B9_IP-%D0%B0%D0%B4%D1%80%D0%B5%D1%81
+   обычно в домашних сетях используются IP адреса вида:
+    10.0.0.1 - 10.255.255.254
+    172.16.0.1 - 172.31.255.254
+    192.168.0.1 - 192.168.255.254
+   вот "что-то подобное" и нужно тут прописать :) тема бесконечная, пишите - отвечу.
+
+   если не используется DHCP, то кроме адреса можно (иногда и нужно) прописать адрес шлюза по умолчанию
+   (default gateway) и адрес(а) DNS сервера (в данном скетче смысла не имеет). шлюз по умолчанию необходим
+   если Modbus master (SCADA) и slave (наше устройство) находятся в разных подсетях и между ними маршрутизатор.
+   такая конфигурация может возникнуть если модули дома, а SCADA на работе. если понадобится, пишите - расскажу,
+   хотя самый простой совет в этом случае использовать DHCP. тема действительно бесконечная, пишите - отвечу.
+*/
+
+// ===
+#include "DStemp.h"
+DSThermometer ds18b20[] = DSPINS;
+
+#include "HDvalve.h"
+Valve valve[] = VALVEPINS;
+
+#ifdef PRESSURE_BMP
+#include "BMP280x.h"
+BMP280x bmp280;
+#endif
+
+// ===
+#define DS_CONVTIME 750   // как часто опрашивать DS18B20 (миллисекунды)
+#define MB_TIMEOUT 50     // как долго можно работать без мастера Модбаса (секунды)
+#define V_OPENTIME 100    // длительность нахождения клапана (старт-стопа) в открытом состоянии (миллисекунды)
+// ===
+#include <ASOLED.h>
+#define LCDX1 1
+#define LCDX2 67
+// ===
+const int dsCount = sizeof(ds18b20) / sizeof(DSThermometer);  // считаем количество DS'ок
+const int vCount = sizeof(valve) / sizeof(Valve);             // считаем количество клапанов
+
+#define hrSECONDS 0                         // регистр-счетчик секунд uptime'а
+#define hrTEMP hrSECONDS + 1                // первый регистр с температурой
+#define hrVALVE hrTEMP + dsCount            // первый регистр с данными для/от клапанов
+#define hrPRESSURE hrVALVE + vCount*3       // регистр давления
+#define hrDSCONVTIME hrPRESSURE + 1         // DEBUG!!, таймаут на преобразование DS (можно менять удаленно)   
+#define hrDSDEBUG hrDSCONVTIME + 1          // DEBUG!!, будем хранить время преобразования каждой DS'ки
+
+// ===
+#ifndef ETHERNET_DHCP     // если компилим _не_ для использования DHCP
+#define MAC0 0x00         // на всякий случай, чтобы по старшему байту MAC адреса можно было определить
+#else
+#define MAC0 0x0A         // с каким параметром скомпилирован скетч - "0A" DHCP или "00" статика
+#endif
+
+#ifdef ETHERNET_ENC28J60
+#include <EtherCard.h>
+#include <ModbusIP_ENC28J60.h>
+#define MAC1 0x28               // если второй байт адреса равен 28 - скомпилировано под enc28j60 
+#define MAC2 0x60               // на всякий случай, чтобы по второму (после старшего) байту MAC адреса
+#endif                          // можно было определить под какой шилд скомпилирован скетч
+
+#ifdef ETHERNET_WIZ5100
+#include <Ethernet.h>
+#include <ModbusIP.h>
+#define MAC1 0x51               // если второй байт адреса равен 51 - скомпилировано под wiz5100
+#define MAC2 0x51
+#endif
+// ===
+
+#include <Modbus.h>
+ModbusIP mb;
+//==================================
+unsigned long msReinit;
+uint16_t msGet, msLcd;
+uint16_t msConvTimeout = DS_CONVTIME;
+const char degC = 223;
+char cbuf[] = {"     "};
+bool mbMasterOK = false;
+const byte mac[] = { ETHERNET_MAC };
+//const uint8_t ip[] = { ETHERNET_IP };
+// ==================================
+
---
+++ mb-TVP-154/functions.ino
@@ -0,0 +1,187 @@
+
+void setupNetMB()
+{
+  //Config Modbus IP
+  LD.printString_6x8("MAC: ", LCDX1, 1);
+  for (byte i = 0; i < 6; ++i)
+  {
+    char mh[2];
+    String macStr = String(mac[i], HEX);
+    macStr.toUpperCase();
+    macStr.toCharArray(mh, 3);
+    LD.printString_6x8(mh);
+    if (i < 5) LD.printString_6x8(":");
+  }
+
+  Serial.print("MAC: ");
+  for (byte i = 0; i < 6; ++i)
+  {
+    Serial.print(mac[i], HEX);
+    if (i < 5) Serial.print(':');
+  }
+  Serial.println();
+
+#ifndef ETHERNET_DHCP
+  const byte ip[] = { ETHERNET_IP };
+  mb.config(mac, ip);
+#else
+  mb.config(mac);
+#endif
+
+#ifdef ETHERNET_ENC28J60
+  ether.printIp("NC28J60 IP: ", ether.myip);
+  ether.printIp("MASK: ", ether.netmask);
+  ether.printIp("GW: ", ether.gwip);
+  //ether.printIp("DNS: ", ether.dnsip);
+  LD.printString_6x8("enc_IP: ", LCDX1, 3);
+  for (byte i = 0; i < 4; ++i)
+  {
+    LD.printNumber((long)ether.myip[i]);
+    if (i < 3) LD.printString_6x8(".");
+  }
+#endif
+
+#ifdef ETHERNET_WIZ5100
+  Serial.print(F("WIZ5100 IP: "));
+  Serial.println(Ethernet.localIP());
+  LD.printString_6x8("wiz_ip: ", LCDX1, 3);
+  for (byte i = 0; i < 4; ++i)
+  {
+    LD.printNumber((long)Ethernet.localIP()[i]);
+    if (i < 3) LD.printString_6x8(".");
+  }
+#endif
+}
+
+void checkMBmaster()
+{
+  // если мастер онлайн - он должен записывать 0 в регистр SECOnSensor
+  // это будет признаком "живости" Мастера Modbus'а для модуля
+  // и наоборот: не 0 в SECONDS - признак "живости" модуля для Мастера
+  // хотя Мастеру логичнее отслеживать "живость" по GetQuality
+  uint16_t secUptime = (uint16_t)(msReinit / 1000);
+  if (mb.Hreg(hrSECONDS) == 0) mb.Hreg(hrSECONDS, secUptime);
+  if ((secUptime - mb.Hreg(hrSECONDS)) < MB_TIMEOUT)
+  {
+    LD.printString_12x16("MB_OK", LCDX2, 6);
+    mbMasterOK = true;
+  }
+  else                // если мастера нет больше MB_TIMEOUT секунд - поднимаем флаг
+  {
+    LD.printString_12x16("  OFF", LCDX2, 6);
+    Serial.print("Master OFFline ");
+    Serial.println(mb.Hreg(hrSECONDS));
+    mbMasterOK = false;
+  }
+}
+
+void initDS(int i)
+{
+  ds18b20[i].init(DS_CONVTIME);
+  mb.addHreg(hrTEMP + i);                // Модбас регистр - значение температуры
+  int pins[] = DSPINS;
+  LD.printNumber((long)pins[i]);
+  if (i < (nSensor - 1)) LD.printString_6x8(", ");
+  String sInfo = "ID " + String(i, DEC) + "|Connected " + String(ds18b20[i].Connected, DEC);
+  Serial.println(sInfo);
+#ifdef DEBUG_INFO
+  mb.addHreg(hrDSDEBUG + i);  // Модбас регистр - длительность преобразования !DEBUG!
+#endif
+}
+
+void initValve(int i)
+{
+  byte hrOpen = hrOPEN + i * 3;         // сдвиг если клапанов больше одного
+  byte hrClose = hrCLOSE + i * 3;
+  byte hrClicks = hrCLICKS + i * 3;
+  valve[i].init();
+  mb.addHreg(hrOpen);         // Valve Rate - мл/час регулирует скорость отбора
+  mb.addHreg(hrClose);         // "базовая" скорость - объем собранный за 1000 кликов
+  mb.addHreg(hrClicks);       // количество кликов с начала работы (для расчета объема)
+  int pins[] = VALVEPINS;
+  LD.printNumber((long)pins[i]);
+  if (i < (nValve - 1)) LD.printString_6x8(", ");
+#ifdef DEBUG_INFO
+  mb.addHreg(hrVALVEDEBUG + i);  // DEBUG !! длительность крайнего открытия
+#endif
+}
+
+void updateDS(int i)
+{
+  float t = ds18b20[i].Temp;
+  mb.Hreg(hrTEMP + i, round( t * 100)); // заносим в регистр Modbus (температура * 100)
+  dtostrf(t, 4, 1, cbuf);
+  LD.printString_12x16(cbuf, 0, (i * 3));
+  String dsInfo = "DS " + String(i, DEC) + ": " + String(t, 2) + " | parasite: " +
+                  String(ds18b20[i].Parasite, DEC) + " | " + String(ds18b20[i].TimeConv, DEC);
+  Serial.println(dsInfo);
+#ifdef DEBUG_INFO
+  mb.Hreg(hrDSDEBUG + i, ds18b20[i].TimeConv);  // DEBUG!
+#endif
+}
+
+void updateValve(int i)
+{
+  byte hrOpen = hrOPEN + i * 3;         // сдвиг если клапанов больше одного
+  byte hrClose = hrCLOSE + i * 3;
+  byte hrClicks = hrCLICKS + i * 3;
+  if (mbMasterOK)
+  {
+    //mb.Hreg(hrVALVEDEBUG + i, valve[i].lastON); // сохраняем залёты по времени (если чаще чем клики клапана??)
+    mb.Hreg(hrClicks, valve[i].Clicks); // сохраняем сколько всего накликали
+  }
+  else
+  {
+    //mb.Hreg(hrLAST_DROP, 0);
+    mb.Hreg(hrOpen, 0);            // если Мастера нет - прекращаем клацать клапаном
+    mb.Hreg(hrClose, 65535);
+  }
+  valve[i].setTime(mb.Hreg(hrClose), mb.Hreg(hrOpen)); // задаем время стопа клапану
+  dtostrf(mb.Hreg(hrOpen), 4, 0, cbuf);
+  LD.printString_6x8(cbuf, LCDX2, i); // показываем это время на дисплее
+  dtostrf(mb.Hreg(hrClose), 5, 0, cbuf);
+  LD.printString_6x8(cbuf, LCDX2 + 32, i); // показываем это время на дисплее
+  String vInfo = "VALVE " + String(i, DEC) + ": On " + String(mb.Hreg(hrOpen), DEC) +
+                 ": Off " + String(mb.Hreg(hrClose), DEC) + " ms | clicks: " + String(valve[i].Clicks, DEC);
+  Serial.println(vInfo);
+#ifdef DEBUG_INFO
+  vInfo = vInfo + "lastOn " + String(valve[i].lastON, DEC) + " ms | Off " + String(valve[i].lastOFF, DEC) + " ms";
+  Serial.println(vInfo);
+  mb.Hreg(hrVALVEDEBUG + i, valve[i].lastON);
+#endif
+}
+
+void updatePressure()
+{
+#ifdef PRESSURE_BMP
+  bmp280.check();
+  mb.Hreg(hrPRESSURE, bmp280.Press_mmHg);
+  dtostrf(mb.Hreg(hrPRESSURE), 5, 0, cbuf);
+  LD.printString_12x16(cbuf, LCDX2, 3);
+  String pInfo = "TEMP " + String(bmp280.Temp_C, 2) + " DegC  PRESS : " + String(bmp280.Press_Pa, DEC) +
+                 " Pa | " + String(bmp280.Press_mmHg, DEC) + " mmHg";
+  Serial.println(pInfo);
+#endif
+
+#ifdef PRESSURE_MPX
+  int dPress_raw = analogRead(MPX5010_PIN);
+  int dPress_mmHg = (dPress_raw * 4 - 160) / 50;
+  mb.Hreg(hrPRESSURE, dPress_mmHg);
+  dtostrf(dPress_mmHg, 5, 0, cbuf);
+  LD.printString_12x16(cbuf, LCDX2, 3);
+  String pInfo = "OverPressure: " + String(dPress_mmHg, DEC) + " mmHg";
+  Serial.println(pInfo);
+#endif
+}
+
+void printFreeRam ()
+{
+  Serial.print(F("Free RAM: "));
+  extern int __heap_start, *__brkval;
+  int v, r;
+  r = (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval);
+  Serial.println(r);
+}
+
+void(*resetFunc) (void) = 0;    // Перезагрузка Ардуины
+
